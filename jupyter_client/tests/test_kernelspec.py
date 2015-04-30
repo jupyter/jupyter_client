d3929f158c704a15b2664b8807fe5e338e874762
Min RK <benjaminrk@gmail.com>
2015-04-30 12:50:38 -0700

    remove SYSTEM_KERNEL_DIRS
    use SYSTEM_JUPYTER_PATH from core instead

--- jupyter_client/tests/test_kernelspec.py
+++ jupyter_client/tests/test_kernelspec.py
@@ -3,6 +3,11 @@ import os
 from os.path import join as pjoin
 import unittest
 
+try:
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch
+    
 from ipython_genutils.testing.decorators import onlyif
 from ipython_genutils.tempdir import TemporaryDirectory
 from jupyter_client import kernelspec
@@ -14,20 +19,29 @@ sample_kernel_json = {'argv':['cat', '{connection_file}'],
 class KernelSpecTests(unittest.TestCase):
     def setUp(self):
         td = TemporaryDirectory()
+        self.env_patch = patch.dict(os.environ, {
+            'JUPYTER_CONFIG_DIR': pjoin(td.name, 'jupyter'),
+            'JUPYTER_DATA_DIR': pjoin(td.name, 'jupyter_data'),
+            'JUPYTER_RUNTIME_DIR': pjoin(td.name, 'jupyter_runtime'),
+        })
+        self.env_patch.start()
         self.addCleanup(td.cleanup)
-        self.sample_kernel_dir = pjoin(td.name, 'kernels', 'Sample')
+        self.sample_kernel_dir = pjoin(td.name, 'jupyter_data', 'kernels', 'Sample')
         os.makedirs(self.sample_kernel_dir)
         json_file = pjoin(self.sample_kernel_dir, 'kernel.json')
         with open(json_file, 'w') as f:
             json.dump(sample_kernel_json, f)
 
-        self.ksm = kernelspec.KernelSpecManager(data_dir=td.name)
+        self.ksm = kernelspec.KernelSpecManager()
 
         td2 = TemporaryDirectory()
         self.addCleanup(td2.cleanup)
         self.installable_kernel = td2.name
         with open(pjoin(self.installable_kernel, 'kernel.json'), 'w') as f:
             json.dump(sample_kernel_json, f)
+
+    def tearDown(self):
+        self.env_patch.stop()
 
     def test_find_kernel_specs(self):
         kernels = self.ksm.find_kernel_specs()
