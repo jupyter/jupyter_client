9af7c29940867f44c786c55841319aad61ff9cc4
Min RK <benjaminrk@gmail.com>
2015-10-05 10:30:54 +0200

    use setuptools entrypoints
    (needed for Windows)

--- setup.py
+++ setup.py
@@ -86,6 +86,12 @@ extras_require = setuptools_args['extras_require'] = {
 
 if 'setuptools' in sys.modules:
     setup_args.update(setuptools_args)
+    setup_args['entry_points'] = {
+        'console_scripts': [
+            'jupyter-kernelspec = jupyter_client.kernelspecapp:KernelSpecApp.launch_instance',
+        ]
+    }
+    setup_args.pop('scripts', None)
 
 if __name__ == '__main__':
     setup(**setup_args)
