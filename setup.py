dcd37a8f68ea0e4ffb6e65d0f6ba04baee0ae1ef
Min RK <benjaminrk@gmail.com>
2017-05-04 16:05:35 +0200

    run tests with pytest

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ install_requires = setuptools_args['install_requires'] = [
 ]
 
 extras_require = setuptools_args['extras_require'] = {
-    'test': ['ipykernel', 'ipython', 'nose_warnings_filters'],
+    'test': ['ipykernel', 'ipython', 'pytest'],
 }
 
 if 'setuptools' in sys.modules:
