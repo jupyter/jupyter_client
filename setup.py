62a7ae1497e2bd457ceb5a0fb90f04784289641a
Craig Citro <craigcitro@google.com>
2017-07-05 13:13:06 -0700

    CR: deps updates (one missing, one spurious).

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ install_requires = setuptools_args['install_requires'] = [
 ]
 
 extras_require = setuptools_args['extras_require'] = {
-    'test': ['ipykernel', 'ipython', 'mock', 'pytest', 'pytest-warnings'],
+    'test': ['ipykernel', 'ipython', 'mock', 'pytest'],
 }
 
 if 'setuptools' in sys.modules:
