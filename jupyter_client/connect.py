a6c8dcbe63aaa326d0cdd3097f77533d34f8fd20
Dawid Manikowski <dawid.manikowski@reef.pl>
2018-01-25 10:34:46 +0100

    Convert to relative import

--- jupyter_client/connect.py
+++ jupyter_client/connect.py
@@ -253,7 +253,7 @@ def tunnel_to_kernel(connection_info, sshserver, sshkey=None):
     (shell, iopub, stdin, hb) : ints
         The four ports on localhost that have been forwarded to the kernel.
     """
-    from jupyter_core.ssh import tunnel
+    from .ssh import tunnel
     if isinstance(connection_info, string_types):
         # it's a path, unpack it
         with open(connection_info) as f:
