8d2f1023f643487b22fdcb630eab3e5ecfc305da
Matthias Bussonnier <bussonniermatthias@gmail.com>
2021-01-12 09:56:58 -0800

    Typo: kenel -> kernel

--- jupyter_client/kernelspec.py
+++ jupyter_client/kernelspec.py
@@ -342,7 +342,7 @@ class KernelSpecManager(LoggingConfigurable):
         return destination
 
     def install_native_kernel_spec(self, user=False):
-        """DEPRECATED: Use ipykernel.kenelspec.install"""
+        """DEPRECATED: Use ipykernel.kernelspec.install"""
         warnings.warn("install_native_kernel_spec is deprecated."
             " Use ipykernel.kernelspec import install.", stacklevel=2)
         from ipykernel.kernelspec import install
