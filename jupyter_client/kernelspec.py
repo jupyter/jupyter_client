1a5b5ad6c0c68e0af02dcd0a5867522e641de127
Kevin Bates <kbates4@gmail.com>
2021-01-12 07:25:53 -0800

    Use list comprehension to filter dirs

--- jupyter_client/kernelspec.py
+++ jupyter_client/kernelspec.py
@@ -201,13 +201,12 @@ class KernelSpecManager(LoggingConfigurable):
 
     def _find_spec_directory(self, kernel_name):
         """Find the resource directory of a named kernel spec"""
-        for kernel_dir in self.kernel_dirs:
-            if os.path.isdir(kernel_dir):
-                files = os.listdir(kernel_dir)
-                for f in files:
-                    path = pjoin(kernel_dir, f)
-                    if f.lower() == kernel_name and _is_kernel_dir(path):
-                        return path
+        for kernel_dir in [kd for kd in self.kernel_dirs if os.path.isdir(kd)]:
+            files = os.listdir(kernel_dir)
+            for f in files:
+                path = pjoin(kernel_dir, f)
+                if f.lower() == kernel_name and _is_kernel_dir(path):
+                    return path
 
         if kernel_name == NATIVE_KERNEL_NAME:
             try:
