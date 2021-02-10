ce3027d7397d7ee29330eb0b76caff2c517466af
Vidar Tonaas Fauske <vidartf@gmail.com>
2021-02-10 11:01:51 +0000

    definitively, surely, use selector loop in tests

--- jupyter_client/tests/conftest.py
+++ jupyter_client/tests/conftest.py
@@ -14,6 +14,8 @@ def event_loop():
     # Make sure we test against a selector event loop
     # since pyzmq doesn't like the proactor loop.
     # This fixture is picked up by pytest-asyncio
+    if os.name == 'nt' and sys.version_info >= (3, 7):
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     loop = asyncio.SelectorEventLoop()
     try:
         yield loop
