c0f12fb6610460f093ccc8d1f98aac376f909025
Min RK <benjaminrk@gmail.com>
2021-08-09 10:02:47 +0200

    pexpect-style wait for output in test_kernelapp
    wait for the expected output to appear, instead of assuming 1 second is enough
    
    this should be both faster (where sleep(1) was enough) and more reliable

--- jupyter_client/tests/test_kernelapp.py
+++ jupyter_client/tests/test_kernelapp.py
@@ -1,10 +1,11 @@
 import os
-import shutil
 import sys
 import time
+from queue import Empty
+from queue import Queue
 from subprocess import PIPE
 from subprocess import Popen
-from tempfile import mkdtemp
+from threading import Thread
 
 
 def _launch(extra_env):
@@ -21,44 +22,80 @@ WAIT_TIME = 10
 POLL_FREQ = 10
 
 
-def test_kernelapp_lifecycle():
+def test_kernelapp_lifecycle(request, tmpdir):
     # Check that 'jupyter kernel' starts and terminates OK.
-    runtime_dir = mkdtemp()
-    startup_dir = mkdtemp()
+    runtime_dir = str(tmpdir.join("runtime").mkdir())
+    startup_dir = str(tmpdir.join("startup").mkdir())
     started = os.path.join(startup_dir, "started")
-    try:
-        p = _launch(
-            {
-                "JUPYTER_RUNTIME_DIR": runtime_dir,
-                "JUPYTER_CLIENT_TEST_RECORD_STARTUP_PRIVATE": started,
-            }
-        )
-        # Wait for start
-        for _ in range(WAIT_TIME * POLL_FREQ):
-            if os.path.isfile(started):
-                break
-            time.sleep(1 / POLL_FREQ)
-        else:
-            raise AssertionError("No started file created in {} seconds".format(WAIT_TIME))
+    p = _launch(
+        {
+            "JUPYTER_RUNTIME_DIR": runtime_dir,
+            "JUPYTER_CLIENT_TEST_RECORD_STARTUP_PRIVATE": started,
+        }
+    )
+    request.addfinalizer(p.terminate)
 
-        # Connection file should be there by now
-        for _ in range(WAIT_TIME * POLL_FREQ):
-            files = os.listdir(runtime_dir)
-            if files:
-                break
-            time.sleep(1 / POLL_FREQ)
-        else:
-            raise AssertionError("No connection file created in {} seconds".format(WAIT_TIME))
-        assert len(files) == 1
-        cf = files[0]
-        assert cf.startswith("kernel")
-        assert cf.endswith(".json")
+    # Wait for start
+    for _ in range(WAIT_TIME * POLL_FREQ):
+        if os.path.isfile(started):
+            break
+        time.sleep(1 / POLL_FREQ)
+    else:
+        raise AssertionError("No started file created in {} seconds".format(WAIT_TIME))
 
-        # Send SIGTERM to shut down
-        time.sleep(1)
+    # Connection file should be there by now
+    for _ in range(WAIT_TIME * POLL_FREQ):
+        files = os.listdir(runtime_dir)
+        if files:
+            break
+        time.sleep(1 / POLL_FREQ)
+    else:
+        raise AssertionError("No connection file created in {} seconds".format(WAIT_TIME))
+
+    assert len(files) == 1
+    cf = files[0]
+    assert cf.startswith("kernel")
+    assert cf.endswith(".json")
+
+    # pexpect-style wait-for-text with timeout
+    # use blocking background thread to read output
+    # so this works on Windows
+    t = time.perf_counter()
+    deadline = t + WAIT_TIME
+    remaining = WAIT_TIME
+
+    stderr = ""
+    q = Queue()
+
+    def _readlines():
+        while True:
+            line = p.stderr.readline()
+            q.put(line.decode("utf8", "replace"))
+            if not line:
+                break
+
+    stderr_thread = Thread(target=_readlines, daemon=True)
+    stderr_thread.start()
+
+    while remaining >= 0 and p.poll() is None:
+        try:
+            line = q.get(timeout=remaining)
+        except Empty:
+            break
+        stderr += line
+        if cf in stderr:
+            break
+        remaining = deadline - time.perf_counter()
+
+    if p.poll() is None:
         p.terminate()
-        _, stderr = p.communicate(timeout=WAIT_TIME)
-        assert cf in stderr.decode("utf-8", "replace")
-    finally:
-        shutil.rmtree(runtime_dir)
-        shutil.rmtree(startup_dir)
+
+    # finish reading stderr
+    stderr_thread.join()
+    while True:
+        try:
+            line = q.get_nowait()
+        except Empty:
+            break
+        stderr += line
+    assert cf in stderr
