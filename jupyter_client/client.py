bf678f2692b8253247e34e46aabd4a19aec8f9b3
Steven Silvester <steven.silvester@ieee.org>
2022-04-17 08:06:42 -0500

    fix handling of context

--- jupyter_client/client.py
+++ jupyter_client/client.py
@@ -93,10 +93,7 @@ class KernelClient(ConnectionFileMixin):
     # The PyZMQ Context to use for communication with the kernel.
     context = Instance(zmq.asyncio.Context)
 
-    _created_context: Bool = Bool(False)
-
     def _context_default(self) -> zmq.asyncio.Context:
-        self._created_context = True
         return zmq.asyncio.Context()
 
     # The classes to use for the various channels
@@ -290,9 +287,6 @@ class KernelClient(ConnectionFileMixin):
         :meth:`start_kernel`. If the channels have been stopped and you
         call this, :class:`RuntimeError` will be raised.
         """
-        # Create the context if needed.
-        if not self._created_context:
-            self.context = self._context_default()
         if iopub:
             self.iopub_channel.start()
         if shell:
@@ -322,8 +316,6 @@ class KernelClient(ConnectionFileMixin):
             self.hb_channel.stop()
         if self.control_channel.is_alive():
             self.control_channel.stop()
-        if self._created_context:
-            self._created_context = False
 
     @property
     def channels_running(self) -> bool:
