c26ce9f8f9c5b697a3f78afeee60ab23e94c3fca
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2022-06-08 20:04:36 +0000

    Publish 7.3.4
    SHA256 hashes:
    
    jupyter_client-7.3.4-py3-none-any.whl: 17d74b0d0a7b24f1c8c527b24fcf4607c56bee542ffe8e3418e50b21e514b621
    
    jupyter_client-7.3.4.tar.gz: aa9a6c32054b290374f95f73bb0cae91455c58dfb84f65c8591912b8f65e6d56

--- jupyter_client/_version.py
+++ jupyter_client/_version.py
@@ -2,7 +2,7 @@ import re
 from typing import List
 from typing import Union
 
-__version__ = "7.3.3"
+__version__ = "7.3.4"
 
 # Build up version_info tuple for backwards compatibility
 pattern = r'(?P<major>\d+).(?P<minor>\d+).(?P<patch>\d+)(?P<rest>.*)'
