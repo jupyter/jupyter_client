bf267bdc4f9a9bdb65d8b9f02b757807cc99c38b
Kevin Bates <kbates4@gmail.com>
2022-11-14 16:36:17 -0800

    Backport PR #879: Reconcile connection information

--- jupyter_client/manager.py
+++ jupyter_client/manager.py
@@ -310,8 +310,9 @@ class KernelManager(ConnectionFileMixin):
         assert self.provisioner is not None
         connection_info = await self.provisioner.launch_kernel(kernel_cmd, **kw)
         assert self.provisioner.has_process
-        # Provisioner provides the connection information.  Load into kernel manager and write file.
-        self._force_connection_info(connection_info)
+        # Provisioner provides the connection information.  Load into kernel manager
+        # and write the connection file, if not already done.
+        self._reconcile_connection_info(connection_info)
 
     _launch_kernel = run_sync(_async_launch_kernel)
 
