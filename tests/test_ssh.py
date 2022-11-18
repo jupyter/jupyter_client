03fe999ea26ee28b499966f8af43e6e828eb9dec
Steven Silvester <steven.silvester@ieee.org>
2022-11-17 19:58:11 -0600

    more coverage

--- tests/test_ssh.py
+++ tests/test_ssh.py
@@ -1,9 +1,17 @@
+import pytest
+
+from jupyter_client.ssh.tunnel import open_tunnel
 from jupyter_client.ssh.tunnel import select_random_ports
 
 
 def test_random_ports():
-    for i in range(4096):
+    for _ in range(4096):
         ports = select_random_ports(10)
         assert len(ports) == 10
         for p in ports:
             assert ports.count(p) == 1
+
+
+def test_open_tunnel():
+    with pytest.raises(RuntimeError):
+        open_tunnel("tcp://localhost:1234", "does.not.exist")
