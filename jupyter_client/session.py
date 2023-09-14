60c1095d9f91dbdf65500e6ddb6883f32b278e4e
Mattias Ellert <mattias.ellert@physics.uu.se>
2023-09-14 03:58:04 +0200

    Do not use datetime.utcnow() that is deprecated in Python 3.12 (#972)
    * Do not use datetime.utcnow() that is deprecated in Python 3.12
    
    Import session.utcnow() into utils instead of reimplementing it
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Remove unneeded noqa
    
    * Add noqa
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: David Brochart <david.brochart@gmail.com>
    Co-authored-by: Steven Silvester <steven.silvester@ieee.org>

--- jupyter_client/session.py
+++ jupyter_client/session.py
@@ -197,7 +197,7 @@ def default_secure(cfg: t.Any) -> None:  # pragma: no cover
 
 def utcnow() -> datetime:
     """Return timezone-aware UTC timestamp"""
-    return datetime.utcnow().replace(tzinfo=utc)  # noqa
+    return datetime.now(utc)
 
 
 # -----------------------------------------------------------------------------
