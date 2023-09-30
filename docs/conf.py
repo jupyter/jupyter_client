43bc70b7a72828417924a504d6aa4adb948f0620
Steven Silvester <steven.silvester@ieee.org>
2023-09-30 09:56:03 -0500

    lint

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import os
 import os.path as osp
 import shutil
 
-from sphinx.util import logging
+from sphinx.util import logging  # type:ignore[import]
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
