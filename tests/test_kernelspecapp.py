b0037a143d4f332ca5dae3bd468ead5c29906a1b
wpk-nist-gov <wpk@nist.gov>
2025-10-15 13:24:35 -0400

    feat: added `--missing` flag to list and remove subcommands (#1056)
    * feat: added `--missing` flag to list and remove subcommands
    
    * chore: better fallback for missing kernels
    
    Now use `shutil.which` to check missing kernels.  This
    catches things like "python" or "bash"
    
    * chore: added basic test to _limit_to_missing func

--- tests/test_kernelspecapp.py
+++ tests/test_kernelspecapp.py
@@ -3,6 +3,9 @@
 # Distributed under the terms of the Modified BSD License.
 import os
 import warnings
+from pathlib import Path
+
+import pytest
 
 from jupyter_client.kernelspecapp import (
     InstallKernelSpec,
@@ -38,6 +41,11 @@ def test_kernelspec_sub_apps(jp_kernel_dir):
     specs = app3.start()
     assert specs and "echo" not in specs
 
+    app4 = ListKernelSpecs(missing_kernels=True)
+    app4.kernel_spec_manager.kernel_dirs.append(kernel_dir)
+    specs = app4.start()
+    assert specs is None
+
 
 def test_kernelspec_app():
     app = KernelSpecApp()
@@ -49,3 +57,63 @@ def test_list_provisioners_app():
     app = ListProvisioners()
     app.initialize([])
     app.start()
+
+
+@pytest.fixture
+def dummy_kernelspecs():
+    import sys
+
+    p = Path.cwd().resolve()
+    # some missing kernelspecs
+    out = {
+        name: {
+            "resource_dir": str(p / name),
+            "spec": {
+                "argv": [
+                    str(p / name / "bin" / "python"),
+                    "-Xfrozen_modules=off",
+                    "-m",
+                    "ipykernel_launcher",
+                    "-f",
+                    "{connection_file}",
+                ],
+                "env": {},
+                "display_name": "Python [venv: dummy0]",
+                "language": "python",
+                "interrupt_mode": "signal",
+                "metadata": {"debugger": True},
+            },
+        }
+        for name in ("dummy0", "dummy1")
+    }
+
+    out["good"] = {
+        "resource_dir": str(p / "good"),
+        "spec": {
+            "argv": [
+                sys.executable,
+                "-Xfrozen_modules=off",
+                "-m",
+                "ipykernel_launcher",
+                "-f",
+                "{connection_file}",
+            ],
+            "env": {},
+            "display_name": "Python [venv: dummy0]",
+            "language": "python",
+            "interrupt_mode": "signal",
+            "metadata": {"debugger": True},
+        },
+    }
+    return out
+
+
+def test__limit_to_missing(dummy_kernelspecs) -> None:
+    from jupyter_client.kernelspecapp import _limit_to_missing
+
+    paths = {k: v["resource_dir"] for k, v in dummy_kernelspecs.items()}
+
+    paths, specs = _limit_to_missing(paths, dummy_kernelspecs)
+
+    assert specs == {k: v for k, v in dummy_kernelspecs.items() if k != "good"}
+    assert paths == {k: v["resource_dir"] for k, v in dummy_kernelspecs.items() if k != "good"}
